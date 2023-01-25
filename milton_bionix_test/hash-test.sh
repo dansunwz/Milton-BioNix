a891d05264c237b69ef5ae93f7db074bd4b60680
dansunwz <118140725+dansunwz@users.noreply.github.com>
2023-01-25 11:21:48 +1100

    Update hash-test.sh

--- milton_bionix_test/hash-test.sh
+++ milton_bionix_test/hash-test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-output=`nix build --impure --no-link --expr 'with import (builtins.getFlake "nixpkgs") {};; fetchurl {url = "https://raw.githubusercontent.com/PapenfussLab/bionix/master/examples/ref.fa"; sha256 = "sha256-V3zqOJFuGtukDRQttK/pGfKofgOlKrridHaWYhGGyWs=";}'`
-if [[ -z $output ]] ; then
+if nix build --impure --no-link --expr 'with import (builtins.getFlake "nixpkgs") {}; fetchurl {url = "https://raw.githubusercontent.com/PapenfussLab/bionix/master/examples/ref.fa"; sha256 = "sha256-V3zqOJFuGtukDRQttK/pGfKofgOlKrridHaWYhGGyWs=";}' ; then
     echo "Your BioNix is working perfectly!"
 else
     echo "Something is wrong, please follow this guide and try to configure BioNix again"
