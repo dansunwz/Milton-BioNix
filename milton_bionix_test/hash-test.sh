3f3db25daa8810a4a9b1366ae606586b8f9767a3
dansunwz <118140725+dansunwz@users.noreply.github.com>
2023-02-09 12:34:34 +1100

    Delete milton_bionix_test directory

--- milton_bionix_test/hash-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if nix build --impure --no-link --expr 'with import (builtins.getFlake "nixpkgs") {}; fetchurl {url = "https://raw.githubusercontent.com/PapenfussLab/bionix/master/examples/ref.fa"; sha256 = "sha256-V3zqOJFuGtukDRQttK/pGfKofgOlKrridHaWYhGGyWs=";}' ; then
-    echo "Your BioNix is working perfectly!"
-else
-    echo "Something is wrong, please follow this guide and try to configure BioNix again"
-fi
